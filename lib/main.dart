ea2df4c9e016d492453e58042c674c5fa474e31e
ezuppardo2I <ezuppardo@2innovation.it>
2025-05-30 17:35:26 +0200

    fixxxx

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_function_declarations_over_variables
 import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart';
 import 'package:dart_template/handlers/api/iot/iot_connection.dart';
+import 'package:dart_template/handlers/api/lobby/GetLobby.dart';
 import 'package:dart_template/handlers/api/lobby/PutLobby.dart';
 import 'package:dart_template/handlers/api/user/GetUser.dart';
 import 'package:dart_template/handlers/api/user/GetUsers.dart';
@@ -15,5 +16,6 @@ void main() async {
     ..registerHandler<AwsApiGatewayEvent>("main.iotConnection", iotConnection)
     ..registerHandler<AwsApiGatewayEvent>("main.patchUserLobbies", patchUser)
     ..registerHandler<AwsApiGatewayEvent>("main.putLobby", putLobby)
+    ..registerHandler<AwsApiGatewayEvent>("main.getLobby", getLobby)
     ..invoke();
 }
