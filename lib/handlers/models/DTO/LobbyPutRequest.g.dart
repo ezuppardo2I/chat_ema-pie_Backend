bd8c62de3c8f02b5b0d59f52c996a31e94bcdd6e
ezuppardo2I <ezuppardo@2innovation.it>
2025-05-30 15:52:15 +0200

    lobby

--- lib/handlers/models/DTO/LobbyPutRequest.g.dart
+++ lib/handlers/models/DTO/LobbyPutRequest.g.dart
@@ -1,20 +1,20 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'Lobby.dart';
+part of 'LobbyPutRequest.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Lobby _$LobbyFromJson(Map<String, dynamic> json) => Lobby(
-      lobbyID: json['lobbyID'] as String,
+LobbyPutRequest _$LobbyPutRequestFromJson(Map<String, dynamic> json) =>
+    LobbyPutRequest(
       name: json['name'] as String,
       userIDs:
           (json['userIDs'] as List<dynamic>).map((e) => e as String).toList(),
     );
 
-Map<String, dynamic> _$LobbyToJson(Lobby instance) => <String, dynamic>{
-      'lobbyID': instance.lobbyID,
+Map<String, dynamic> _$LobbyPutRequestToJson(LobbyPutRequest instance) =>
+    <String, dynamic>{
       'name': instance.name,
       'userIDs': instance.userIDs,
     };
