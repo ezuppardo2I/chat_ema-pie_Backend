22779901563024cd92af7eee58fae1ab29ee6f09
ezuppardo2I <ezuppardo@2innovation.it>
2025-06-03 15:10:10 +0200

    fix put

--- lib/handlers/models/DTO/MessagePutRequest.dart
+++ lib/handlers/models/DTO/MessagePutRequest.dart
@@ -4,13 +4,11 @@ part 'MessagePutRequest.g.dart';
 
 @JsonSerializable(explicitToJson: true, includeIfNull: false)
 class MessagePutRequest {
-  String messageID;
   String userID;
   String lobbyID;
   String messageText;
 
   MessagePutRequest({
-    required this.messageID,
     required this.userID,
     required this.lobbyID,
     required this.messageText,
